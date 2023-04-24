5c2ac9976adc1cfb0709fb3677109f34203b6bdf
Samuel Silva <samuel7silva@outlook.com>
2023-04-24 09:32:56 -0300

    feat: create load button

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 interface User {
@@ -12,19 +13,32 @@ interface AuthPageProps {
 }
 
 function AuthPage({ onAuth }: AuthPageProps): JSX.Element {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const authenticateUser = async (username: string): Promise<User> => {
+    const response = await axios.post<User>(
+      `${import.meta.env.VITE_APP_BASE_URL}/authenticate`,
+      {
+        username,
+      }
+    );
+    return response.data;
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const inputElement = e.currentTarget.elements.namedItem(
       'username'
     ) as HTMLInputElement | null;
     if (inputElement) {
       const { value } = inputElement;
-      axios
-        .post<User>(`${import.meta.env.VITE_APP_BASE_URL}/authenticate`, {
-          username: value,
-        })
-        .then((r) => onAuth({ ...r.data, secret: value }))
-        .catch((err) => console.log('Auth Error', err));
+      setIsLoading(true);
+      try {
+        const user = await authenticateUser(value);
+        onAuth({ ...user, secret: value });
+      } catch (err) {
+        console.log('Auth Error', err);
+      } finally {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -59,11 +73,36 @@ function AuthPage({ onAuth }: AuthPageProps): JSX.Element {
             id="username"
             required
           />
-          <input
-            className="mt-3  w-full rounded-sm p-2 text-white cursor-pointer bg-green-500"
+          <button
+            className="mt-3 flex items-center justify-center h-9 w-full rounded-sm p-2 text-white cursor-pointer bg-green-500"
             type="submit"
-            value="Enter"
-          />
+          >
+            {' '}
+            {!isLoading ? (
+              'Enter'
+            ) : (
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25 text"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            )}
+          </button>
         </form>
       </div>
 
